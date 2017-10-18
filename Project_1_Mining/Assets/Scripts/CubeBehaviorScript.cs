ad493e2443e71cf26db8fd0c816ceef3bd6e4802
DESKTOP-8T8C0L2\eric <eap17@hampshire.edu>
2017-10-18 15:49:18 -0400

    Mining Project Final

--- Project_1_Mining/Assets/Scripts/CubeBehaviorScript.cs
+++ Project_1_Mining/Assets/Scripts/CubeBehaviorScript.cs
@@ -1,16 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CubeBehaviorScript : MonoBehaviour {
+    string cubeType;
+    public Text scoreText;
+    void SetScoreText () {
+        scoreText.text = "Score: " + GameController.playerScore.ToString();
+    }
 
-	// Use this for initialization
-	void Start () {
-		
+    // Use this for initialization
+    void Start () {
+		if (GameController.cubeColor == 1) { cubeType = "gold"; }
+        if (GameController.cubeColor == 2) { cubeType = "silver"; }
+        if (GameController.cubeColor == 3) { cubeType = "bronze"; }
 	}
+    private void OnMouseDown()
+    {
+        if (cubeType == "gold")
+        {
+            GameController.goldSupply -= 1;
+            GameController.playerScore += GameController.goldScore;
+            SetScoreText();
+        }
+        if (cubeType == "silver")
+        {
+            GameController.silverSupply -= 1;
+            GameController.playerScore += GameController.silverScore;
+            SetScoreText();
+        }
+        if (cubeType == "bronze")
+        {
+            GameController.bronzeSupply -= 1;
+            GameController.playerScore += GameController.bronzeScore;
+            SetScoreText();
+        }
+        Destroy(gameObject);
+    }
+    // Update is called once per frame
+    void Update () {
 	
-	// Update is called once per frame
-	void Update () {
-		
 	}
 }
--- Project_1_Mining/Assets/Scripts/GameController.cs
+++ Project_1_Mining/Assets/Scripts/GameController.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
     int bronzePlayer = 0;
+    public Text scoreText;
     int silverPlayer = 0;
     //Public makes the variable editable in Unity
     public int miningSpeed = 3;
@@ -12,56 +14,67 @@ public class GameController : MonoBehaviour {
     public GameObject myCube;
     //Vector3 creates a x, y, and z position
     Vector3 cubePosition;
-    public float xPosition;
-    GameObject currentCube;
+    public GameObject currentCube;
     int goldPlayer;
-    int goldSupply;
-    int bronzeSupply;
-    int silverSupply;
+    public static int goldSupply;
+    public static int bronzeSupply;
+    public static int silverSupply;
+    public static int playerScore;
+    public static int cubeColor;
+    public static int goldScore = 100;
+    public static int silverScore = 10;
+    public static int bronzeScore = 1;
     void MakeCube()
     {
-
+        cubePosition = new Vector3(Random.Range (-5f, 5f), Random.Range (-4f, 4f), 0f);
+        //Instantiate makes a cube appear using (WhatToInstantiate, WhereToMakeIt, Rotation)
+        currentCube = Instantiate(myCube, cubePosition, Quaternion.identity);
+        
     }
 
 	// Use this for initialization
 	void Start () {
         timeToMine = miningSpeed;
-        //Sets where the cubes will appear, can use Random.Range (min: x, max: y)
-        xPosition = 0;
-	}
+        scoreText.text = "Score: " + playerScore.ToString ();
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		if (Time.time > timeToMine)
         {
-            if (bronzePlayer == 2 && silverSupply == 2){
+            if (bronzeSupply == 2 && silverSupply == 2){
                 goldPlayer += 1;
                 goldSupply += 1;
-                cubePosition = new Vector3(0f + goldPlayer + goldPlayer, 6f, 0f);
-                currentCube = Instantiate(myCube, cubePosition, Quaternion.identity);
+                MakeCube();
                 currentCube.GetComponent<Renderer>().material.color = Color.yellow;
+                //Gold = 1, Silver =2, Bronze = 3
+                cubeColor = 1;
             }
-            if (bronzeSupply >= 4)
+            else if (bronzeSupply >= 4)
             {
-                    silverPlayer += 1;
-                    silverSupply += 1;
-                    cubePosition = new Vector3(0f + silverPlayer + silverPlayer, 4f, 0f);
-                    currentCube = Instantiate (myCube, cubePosition, Quaternion.identity);
-                    currentCube.GetComponent<Renderer>().material.color = Color.gray; 
+                silverPlayer += 1;
+                silverSupply += 1;
+                MakeCube();
+                currentCube.GetComponent<Renderer>().material.color = Color.gray;
+                //Gold = 1, Silver =2, Bronze = 3
+                cubeColor = 2;
+                
             }
-            if (bronzePlayer < 4) {
+            else if (bronzeSupply < 4) {
                 bronzePlayer += 1;
                 bronzeSupply += 1;
-                cubePosition = new Vector3(0f + bronzePlayer + bronzePlayer, 2f, 0f);
-                //Instantiate makes a cube appear using (WhatToInstantiate, WhereToMakeIt, Rotation)
-                currentCube = Instantiate (myCube, cubePosition, Quaternion.identity);
+                MakeCube();
                 //This can assign a color using Unity's renderer
                 currentCube.GetComponent<Renderer>().material.color = Color.red;
+                //Gold = 1, Silver =2, Bronze = 3
+                cubeColor = 3;
             }
             //Wait miningSpeed seconds to mine more ore
             timeToMine += miningSpeed;
             //Tell the player how many ore they have
-            print("Bronze:"+bronzePlayer + ".... Silver:"+silverPlayer);
-       }
-	}
+            print("Score:" + playerScore);
+            
+        }
+        scoreText.text = "Score: " + playerScore.ToString();
+    }
 }
